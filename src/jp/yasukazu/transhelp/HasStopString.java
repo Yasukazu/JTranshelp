35507690529495ee85c90ea44752723392773a18
Yasukazu <makino@yasukazu.jp>
2018-08-31 15:54:18 +0900

    wide punct chars

--- src/jp/yasukazu/transhelp/HasStopString.java
+++ src/jp/yasukazu/transhelp/HasStopString.java
@@ -1,5 +1,6 @@
 package jp.yasukazu.transhelp;
-
+import java.util.Arrays;
+// 2018/8/31 : wide stop characters
 import java.util.EnumSet;
 import java.util.HashSet;
 import java.util.Set;
@@ -11,10 +12,21 @@ import java.util.Set;
  */
 public class HasStopString {
 	enum stopChars {
+	    CJKSPC('\u3000'),
+	    TOUTEN('\u3001'),
 	    KUTEN('\u3002'),
-	    QSTN('?'),
 	    EXCL('!'),
-	    FULL('.'),
+	    QSTN('?'),
+	    COMMA(','),
+	    FLSTP('.'),
+	    COLON(':'),
+	    SEMI(';'),
+	    WEXCL('\uFF01'),
+	    WQSTN('\uFF1F'),
+	    WCOMMA('\uFF0C'),
+	    WFLSTP('\uFF0E'),
+	    WCOLON('\uFF1A'),
+	    WSEMI('\uFF1B'),
 	    ;
 	    private char ch;
 	    stopChars(char ch){
@@ -25,9 +37,26 @@ public class HasStopString {
 	    }		
 	}
 	static Set<Character> stopCharSet;
+	static String stopCharStr;
+	static stopChars[] wStopChars = {stopChars.KUTEN, stopChars.WEXCL, stopChars.WFLSTP, stopChars.WQSTN};
+	static Set<Character> wStopCharSet;
+	static String wStopCharStr;
+	static Set<Character> fullstopCharSet;
+	static String fullstopCharStr;
+	static stopChars[] fullstopChars = {stopChars.KUTEN, stopChars.WEXCL, stopChars.WFLSTP, stopChars.WQSTN};
 	static {
 		stopCharSet = new HashSet<Character>();
 		EnumSet.allOf(stopChars.class).forEach(it -> stopCharSet.add(it.ch));
+		StringBuilder sb = new StringBuilder();
+		for (Character ch : stopCharSet)
+			sb.append(ch);
+		stopCharStr = sb.toString();
+		wStopCharSet = new HashSet<Character>();
+		Arrays.asList(wStopChars).forEach(sc -> wStopCharSet.add(sc.ch));
+		sb.setLength(0);
+		for (Character ch : wStopCharSet)
+			sb.append(ch);
+		wStopCharStr = sb.toString();		
 	}
 
 	public static HasStopString toHasStopString(String str) {
@@ -45,11 +74,12 @@ public class HasStopString {
 		this.str = str;
 		this.stop = stop;
 	}
-	/*
+	
 	HasStopString(String str, boolean has_stop){
 		this.str = has_stop ? str.substring(0, str.length()-1) : str;
 		this.stop = has_stop ? str.charAt(str.length()-1) : 0;
-	}*/
+	}
+	
 	public String getStr() {
 		return this.str;
 	}
@@ -67,3 +97,4 @@ public class HasStopString {
 	}
 
 }
+
