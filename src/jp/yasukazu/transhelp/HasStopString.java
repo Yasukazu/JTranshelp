df8d333b6c90a2cf99d46d63c95df6ea29a79e3d
Yasukazu Makino <makino@yasukazu.jp>
2018-08-31 11:36:41 +0900

    enum stopChars, stopCharSet, HasStopString(String)
    enum stopChars, Set stopCharSet, HasStopString(String)

--- src/jp/yasukazu/transhelp/HasStopString.java
+++ src/jp/yasukazu/transhelp/HasStopString.java
@@ -1,12 +1,57 @@
 package jp.yasukazu.transhelp;
 
+import java.util.EnumSet;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * String with stip character like period(aka: full stop), question mark, exclamation mark
+ * @author yasukazu
+ *
+ */
 public class HasStopString {
-	public String str;
-	public char stop;
+	enum stopChars {
+	    KUTEN('\u3002'),
+	    QSTN('?'),
+	    EXCL('!'),
+	    FULL('.'),
+	    ;
+	    private char ch;
+	    stopChars(char ch){
+	      this.ch = ch;
+	    }
+	    public char getChar(){
+	      return this.ch;
+	    }		
+	}
+	static EnumSet<stopChars> stopSet;
+	static Set<Character> stopCharSet;
+	static {
+		stopCharSet = new HashSet<Character>();
+		//EnumSet.allOf(stopChars.class).
+		stopSet.forEach(it -> stopCharSet.add(it.ch));
+	}
+
+	public static HasStopString toHasStopString(String str) {
+		str = str.trim();
+		if (str.length() == 0)
+			return new HasStopString("", '\0');
+		char lastc = str.charAt(str.length() - 1);
+		if (!stopCharSet.contains(lastc))
+			return new HasStopString(str, '\0');			
+		return new HasStopString(str.substring(0, str.length() - 1), lastc);
+	}
+	String str;
+	char stop;
+	HasStopString(String str, char stop) {
+		this.str = str;
+		this.stop = stop;
+	}
+	/*
 	HasStopString(String str, boolean has_stop){
 		this.str = has_stop ? str.substring(0, str.length()-1) : str;
 		this.stop = has_stop ? str.charAt(str.length()-1) : 0;
-	}
+	}*/
 	public String getStr() {
 		return this.str;
 	}
