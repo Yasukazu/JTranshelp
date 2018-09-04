c70b8346a5eee061ab98c91b28f648b53f792b1c
Yasukazu <makino@yasukazu.jp>
2018-09-04 19:06:32 +0900

    charCmdEnumMap

--- src/jp/yasukazu/transhelp/Editor.java
+++ src/jp/yasukazu/transhelp/Editor.java
@@ -1,12 +1,16 @@
 package jp.yasukazu.transhelp;
 // 2018/8/30 YtM @ yasukazu.jp
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.EnumSet;
+import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
+import jp.yasukazu.transhelp.Transhelp.punct;
+
 import java.util.HashSet;
 
 @SuppressWarnings("serial")
@@ -31,32 +35,43 @@ public class Editor extends ArrayList<Object> {
 	  }
 	  static Set<Character> cmdCharSet;
 	  static String cmdCharStr;
-	static Set<Character> cmdchset;
 	static Map<Character, Cmd> cmdKeyMap;
+	static Map<Character, Character> cmdCharMap;
+	static Map<Character, cmdEnum> charCmdEnumMap;
 	static {
-		cmdchset = new HashSet<Character>();
-		EnumSet.allOf(cmdEnum.class).forEach(it -> cmdchset.add(it.ch));
+		cmdCharSet = new HashSet<Character>();
+		EnumSet.allOf(cmdEnum.class).forEach(it -> {
+			cmdCharSet.add(it.ch);
+			cmdCharSet.add(it.wch);
+		});
+		cmdKeyMap = new HashMap<Character, Cmd>();
+		EnumSet.allOf(cmdEnum.class).forEach(it -> {
+			cmdKeyMap.put(it.ch, it.cmd);
+		});
+		cmdCharMap = new HashMap<Character, Character>();
+		EnumSet.allOf(cmdEnum.class).forEach(it -> {
+			cmdCharMap.put(it.wch, it.ch);
+			cmdCharMap.put(it.ch, it.ch);
+		});
+		charCmdEnumMap = new HashMap<Character, cmdEnum>();
+		EnumSet.allOf(cmdEnum.class).forEach(it -> {
+			charCmdEnumMap.put(it.wch, it);
+			charCmdEnumMap.put(it.ch, it);
+		});
 	}
 	public interface Cmd {
-		public void exec(List<Object> l) throws TranshelpException;
+		public void exec(List<Object> l, char ch) throws TranshelpException;
 	}
 	static class Void implements Cmd {
 		@Override
-		public void exec(List<Object> lst) {
+		public void exec(List<Object> lst, char ch) {
 		}
 	}
 	static class Reverse implements Cmd {
 		@Override
-		public void exec(List<Object> lst) throws TranshelpException {
-			boolean includes  = false;
-			for (Object it : lst) 
-				if (it instanceof Character) { // Character is command
-					includes = true;
-					break;
-				}
-			if (!includes)
-				return;
-			RunCutter rc = new RunCutter(lst, cmdEnum.REVERSE.getChar());
+		public void exec(List<Object> lst, char ch) throws TranshelpException {
+
+			RunCutter rc = new RunCutter(lst, ch);//cmdEnum.REVERSE.ch);
 			int[] pos_len;
 			try {
 				while ((pos_len = rc.get())!= null) {
@@ -163,49 +178,91 @@ public class Editor extends ArrayList<Object> {
 		for (int i = 0; i < list.size(); ++i) {
 			Object it = list.get(i);
 			if (it instanceof String && ((String)it).length() == 1
-		&& cmdchset.contains(((String)it).charAt(0))) {
+		&& cmdCharSet.contains(((String)it).charAt(0))) {
 				char ch = ((String)it).charAt(0);
 				list.set(i, ch);
 			}
 			
 		}
-		/*
-		list.stream().map(it -> 
-		it instanceof String && ((String)it).length() == 1
-		&& cmdchset.contains(((String)it).charAt(0)) ? 
-				((String)it).charAt(0) : it 
-		)
-		.collect(Collectors.toList()); */
 	}
+	
+	public static Enumeration<List<Object>> idgcommaYsplit(List<Object> list) {
+		class EnumIdgComma implements Enumeration<List<Object>> {
+			List<Object> list;
+			int pos;
+			int cur;
+			int size;
+			EnumIdgComma(List<Object> list) {
+				this.list = list;
+				pos = cur = 0;
+				this.size = list.size();
+			}
+
+			@Override
+			public boolean hasMoreElements() {
+				return cur < size;
+			}
+
+			@Override
+			public List<Object> nextElement() {
+				if (!hasMoreElements())
+					return null;
+				List<Object> rList;
+				int idx = cur;
+				for (; idx < size; ++idx) {
+					Object obj = list.get(idx);
+					if (obj instanceof Character && ((Character)obj) == punct.IDGCOMMA.ch) {
+						rList = new ArrayList<>(list.subList(cur, idx));
+						cur += idx + 2;
+						return rList;
+					}
+				}
+    			rList = list.subList(cur, size);
+	  			cur = size;
+				return rList;
+			}			
+		}
+		return new EnumIdgComma(list);
+	}
+
 	public void recurEdit(cmdEnum ck) throws TranshelpException {
 		class Recur {
 			cmdEnum ck;
 			Recur(cmdEnum ck) {
 				this.ck = ck;
 			}
-			void recurExec(List<Object> list, int nest) throws TranshelpException {
-				enchar_cmd(list);
-				try {
-				  ck.getCmd().exec(list);
+			void recurExec(List<Object> lst, int nest) throws TranshelpException {
+				Enumeration<List<Object>> idgcomma_split = idgcommaYsplit(lst);
+				List<List<Object>> idgcomma_split_list = new ArrayList<>();
+				while (idgcomma_split.hasMoreElements()) 
+					idgcomma_split_list.add(idgcomma_split.nextElement());
+				List<Object> removeList = new ArrayList<>();
+				for (int i = 0; i < idgcomma_split_list.size(); ++i) {
+					List<Object> list = idgcomma_split_list.get(i);
+					try {
+						//enchar_cmd(list);
+					    ck.cmd.exec(list, ck.ch);
+						for (int j = 0; j < list.size(); ++j) {
+							Object it = list.get(j);
+							if (it instanceof EnclosedArray) {
+								recurExec((EnclosedArray)it, nest + 1);
+							}			
+							else if (it instanceof Character && ((Character)it) == ck.ch)
+								removeList.add(it); //list.remove(it);
+						}
+					  
+					}
+					catch (TranshelpException e) {
+						throw new TranshelpException(e.getMessage());
+					}
+
+					int rsize = removeList.size();
+					for (int k = 0; k < rsize; ++k) {
+						list.remove(removeList.get(k));
+					}
+					removeList.clear();
 				}
-				catch (TranshelpException e) {
-					throw new TranshelpException(e.getMessage());
-				}				
-				list.removeAll(Arrays.asList(ck.getChar()));
-				for (Object item : list) {
-					if (item instanceof EnclosedArray) {
-						recurExec((EnclosedArray)item, nest + 1);
-					}			
-				}									
-			}
-			void enchar_cmd(List<Object> list) {
-				for (int i = 0; i < list.size(); ++i) {
-					Object it = list.get(i);
-					if (it instanceof String && ((String)it).length() == 1
-							&& ( ((String)it).charAt(0) == ck.ch  || ((String)it).charAt(0) == ck.wch ) ){
-						list.set(i, ck.ch); // accept full-width command
-					}					
-				}
+				
 			}
 		}
 		Recur rc = new Recur(ck);
@@ -215,29 +272,6 @@ public class Editor extends ArrayList<Object> {
 		catch(TranshelpException e) {
 			throw new TranshelpException(e.getMessage());
 		}
-	}
-	/**
-	 * 
-	 * @param cmd
-	 * @throws TranshelpException
-	 */
-	public void recur_edit(Cmd cmd) throws TranshelpException {
-		_recur_edit(this, cmd, 0);
-	}
-	static final int MAXNEST = 9;
-	void _recur_edit(List<Object> lst, Cmd cmd, int nst) throws TranshelpException {
-		if (nst > MAXNEST)
-			throw new TranshelpException(String.format("recur %d edit", nst));
-		enchar_cmd(lst);
-		cmd.exec(lst);
-		for (Object item : lst) {
-			if (item instanceof EnclosedArray) {
-				_recur_edit((EnclosedArray)item, cmd, nst + 1);
-			}			
-		}					
-	}
-	public void do_reverse() throws TranshelpException {
-		recur_edit(new Reverse());
 	}
 
 	public String toString() {
