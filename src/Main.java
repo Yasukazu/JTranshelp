57b105f8d41c39b67c26c85fbab09723bf670f62
Yasukazu <makino@yasukazu.jp>
2018-09-07 22:09:20 +0900

    RunIter test

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.io.IOException;
@@ -9,6 +11,17 @@ import jp.yasukazu.transhelp.*;
 
 class Main {
   public static void main(String[] args) {
+	  List<Object> run_iter_sample = new ArrayList<>();
+	  run_iter_sample.addAll(Arrays.asList("a b".split(" ")));
+	  run_iter_sample.add(Editor.cmdEnum.REVERSE);
+	  run_iter_sample.addAll(Arrays.asList("c d".split(" ")));
+	  run_iter_sample.add(Editor.cmdEnum.REVERSE);
+	  run_iter_sample.addAll(Arrays.asList("e f".split(" ")));
+	  Editor.RunIter run_iter = new Editor.RunIter(run_iter_sample, Editor.cmdEnum.REVERSE);
+	  List<List<Object>> output_list = new ArrayList<>();
+	  for (List<Object> part_list : run_iter) {
+		  output_list.add(part_list);
+	  }
     String usage_filename = "README.md";
     String filename = "input.txt";
     try (Stream<String> stream = Files.lines(Paths.get(filename), StandardCharsets.UTF_8);
