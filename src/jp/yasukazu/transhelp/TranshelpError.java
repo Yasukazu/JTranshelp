e14978eee13c4649e8de76b3d912d26e566efe4e
Yasukazu <makino@yasukazu.jp>
2018-09-07 22:08:10 +0900

    RuntimeException

---
+++ src/jp/yasukazu/transhelp/TranshelpError.java
@@ -0,0 +1,13 @@
+package jp.yasukazu.transhelp;
+
+public class TranshelpError extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2754764587813740608L;
+	public TranshelpError(String msg) {
+		super(msg);
+	}
+
+}
