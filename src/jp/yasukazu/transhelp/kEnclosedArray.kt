8e8083796faad6d9cb9c1fe1cf6764a8252cae7f
Yasukazu <makino@yasukazu.jp>
2018-09-23 01:15:09 +0900

    1st commit add

---
+++ src/jp/yasukazu/transhelp/kEnclosedArray.kt
@@ -0,0 +1,37 @@
+package jp.yasukazu.transhelp
+
+import jp.yasukazu.transhelp.EnBlock.BracketPair
+import java.util.ArrayList
+
+class EnclosedArray2 : ArrayList<Any> {
+
+    var pair: BracketPair
+    val begin: Char
+        get() = pair.begin
+    val end: Char
+        get() = pair.end
+
+    constructor(list: List<Any>, pair: BracketPair) : super(list) {
+        this.pair = pair
+    }
+
+    constructor(list: List<Any>) : super(list) {
+        this.pair = BracketPair.NUL
+    }
+
+    constructor() : super() {
+        this.pair = BracketPair.NUL
+    }
+
+    fun insert() {
+        if (pair != BracketPair.NUL) {
+            this.add(0, "" + pair.begin)
+            this.add("" + pair.end)
+            pair = EnBlock.BracketPair.NUL
+        }
+    }
+
+    companion object {
+        private val serialVersionUID = 1002001L
+    }
+}
