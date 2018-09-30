52c06efc60be9f32bfad8b0fd2ec4a7a89fe8ddf
Yasukazu <makino@yasukazu.jp>
2018-10-01 00:19:24 +0900

    usageStream : TransHelp::class.java.getResourceAsStream

--- src/kMain.kt
+++ src/kMain.kt
@@ -3,17 +3,27 @@ import java.nio.file.Files
 import java.nio.file.Paths
 import java.io.IOException
 import jp.yasukazu.transhelp.*
+import org.apache.commons.cli.Options
+import org.apache.commons.cli.DefaultParser
 import kotlin.streams.toList
 
+
 fun main(args: Array<String >) {
-    val usage_filename = "README.md"
-    val filename = "input.txt"
-    try {
-        Files.lines(Paths.get(filename), StandardCharsets.UTF_8).use { stream ->
-            Files.lines(Paths.get(usage_filename), StandardCharsets.UTF_8).use { usage_stream ->
-                usage_stream.forEach { it -> println(it) }
-                val lines = stream.map<String> { it -> it.trim()} .toList()
-                val thelp = KTranshelp(lines)
+    var options = Options()
+    options.addOption("f", true, "Input text file")
+    val parser = DefaultParser()
+    val cmd = parser.parse(options, args)
+    val usage_filename = "USAGE.md"
+    val usageStream = TransHelp::class.java.getResourceAsStream(usage_filename)
+    val filename = if (cmd.hasOption("f") && cmd.getOptionValue("f") != null) cmd.getOptionValue("f") else "input.txt"
+    try { var path = Paths.get(filename)
+        Files.lines(path, StandardCharsets.UTF_8).use { stream -> //
+            //Files.lines(Paths.get(usage_filename), StandardCharsets.UTF_8).use { usage_stream ->
+            println("Usage:")
+            usageStream.bufferedReader().forEachLine(::println)
+                println()
+                val lines = stream.map<String> { it -> it.trim()} //.toList()
+                val thelp = TransHelp(lines.toList())
 
                 val editorList = thelp.editAll()
                 for (i in thelp.indices) {
@@ -22,7 +32,7 @@ fun main(args: Array<String >) {
                     val edt = editorList[i]
                     println("Edit: " + edt.toString() + edt.stop)
                 }
-            }
+
         }
     } catch (e: IOException) {
         print("IOException occured: ")
