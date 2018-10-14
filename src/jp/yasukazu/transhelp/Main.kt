0a2c0de821a1c1a94febba748f80ff8f10930829
Yasukazu <makino@yasukazu.jp>
2018-10-14 14:40:51 +0900

    added manifest.mf

--- src/jp/yasukazu/transhelp/Main.kt
+++ src/jp/yasukazu/transhelp/Main.kt
@@ -13,6 +13,16 @@ import kotlin.system.exitProcess
 
 
 class Main(val args: Array<String >) {
+    var inputFileName = ""
+    init {
+        if (args.isEmpty()) {
+            usage().forEach(::println)
+            throw TranshelpException("Needs input text or command 'f' : input file")
+        }
+        if (args[0][0] == 'f' && args.size >= 2) {
+            inputFileName = args[1]
+        }
+    }
     fun usage():List<CharSequence> {
         val bundle = ResourceBundle.getBundle("jp.yasukazu.transhelp.usage", UTF8_ENCODING_CONTROL)
         var key = "TOTAL"
@@ -35,6 +45,7 @@ class Main(val args: Array<String >) {
         return emptyList()
     }
     fun run() {
+        /*
         var optionMap = mutableMapOf(Pair('f', ""))
         args.filter { it.length >= 3 && it.contains('=') }
                 .forEach {
@@ -46,35 +57,40 @@ class Main(val args: Array<String >) {
                         optionMap[optChar] = arg
                 }
         val filename = if (optionMap.contains('f') && optionMap['f']!!.isNotEmpty()) optionMap['f'] else "input.txt"//if (cmd.hasOption("f") && cmd.getOptionValue("f") != null) cmd.getOptionValue("f") else "input.txt"
-        val startDir = System.getProperty("user.dir")
-        var path = Paths.get(startDir, filename)
-        if (!Files.exists(path)) {
-            println("$path does not exits.")
-            exitProcess(1)
-        }
-        try {
-            Files.lines(path, StandardCharsets.UTF_8).use { stream ->
-                //
-                //Files.lines(Paths.get(usage_filename), StandardCharsets.UTF_8).use { usage_stream ->
-                println("Usage:")
-                usage().forEach(::println)
-                //usageStream.bufferedReader().forEachLine(::println)
-                println()
-                val lines = stream.map<String> { it -> it.trim() } //.toList()
-                val tHelp = TransHelp(lines.toList())
-
-                val editorList = tHelp.editAll()
-                for (i in tHelp.indices) {
-                    val hsStr = tHelp[i]
-                    println("Orig: " + hsStr.str + hsStr.stop)
-                    val edt = editorList[i]
-                    println("Edit: " + edt.toString() + edt.stop)
-                }
+        val startDir = System.getProperty("user.dir") */
+        var inputLines = listOf(args[0])
+        if (inputFileName.isNotEmpty()) {
+            var path = Paths.get(inputFileName)//startDir, filename)
+            if (!Files.exists(path)) {
+                println("$path does not exits.")
+                exitProcess(1)
             }
-        } catch (e: IOException) {
-            print("IOException occured: ")
-            e.printStackTrace()
-        } catch (e: TranshelpException) {
+            try {
+                Files.lines(path, StandardCharsets.UTF_8).use { stream ->
+                    inputLines = stream.toList()
+                }
+            } catch (e: IOException) {
+                print("IOException occured: ")
+                e.printStackTrace()
+            }
+        }//
+                //Files.lines(Paths.get(usage_filename), StandardCharsets.UTF_8).use { usage_stream ->
+                //println("Usage:")
+                //usage().forEach(::println)
+                //usageStream.bufferedReader().forEachLine(::println)
+                //println()
+         //       val lines = stream.map<String> { it -> it.trim() } //.toList()
+        try {
+            val tHelp = TransHelp(inputLines)
+            val editorList = tHelp.editAll()
+            for (i in tHelp.indices) {
+                val hsStr = tHelp[i]
+                println("Orig: " + hsStr.str + hsStr.stop)
+                val edt = editorList[i]
+                println("Edit: " + edt.toString() + edt.stop)
+            }
+        }
+        catch (e: TranshelpException) {
             println("Sentence is not proper format.")
             e.printStackTrace()
         }
