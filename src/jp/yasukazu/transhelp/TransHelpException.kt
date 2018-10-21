3e33378a342366122b89382671c7e0d67c4efca1
Yasukazu <makino@yasukazu.jp>
2018-10-21 22:37:41 +0900

    Throwable(msg)

---
+++ src/jp/yasukazu/transhelp/TransHelpException.kt
@@ -0,0 +1,8 @@
+package jp.yasukazu.transhelp
+
+class TransHelpException(msg: String) : Throwable(msg) {//Exception(msg) {
+    companion object {
+        private val serialVersionUID = 2754764587813740608L
+    }
+
+}
